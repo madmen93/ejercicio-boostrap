60c258b100a3f1954b71fade4cee7d558f5bf2e6
Monica Daza <158379665+madmen93@users.noreply.github.com>
2024-07-16 20:54:12 -0500

    Se creó página de contacto y formulario

---
+++ servicioFreelance/servicioFreelance/Contacto.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="Contacto.aspx.cs" Inherits="servicioFreelance.Contacto" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col">
+            <h2>Contáctame</h2>
+            <div class="mb-3">
+                <label for="txbNombreApellido" class="form-label">Nombre:</label>
+                <asp:TextBox ID="txbNombreApellido" CssClass="form-control" runat="server" type="text" placeholder="Name Lastname"></asp:TextBox>
+            </div>
+            <div class="mb-3">
+                <label for="txbEmailAdress" class="form-label">Email:</label>
+                <asp:TextBox ID="txbEmailAdress" CssClass="form-control" runat="server" type="email" placeholder="name@example.com"></asp:TextBox>
+            </div>
+            <div class="mb-3">
+                <label for="txbAsunto" class="form-label">Asunto:</label>
+                <asp:TextBox ID="txbAsunto" CssClass="form-control" runat="server" type="text"></asp:TextBox>
+            </div>
+            <div class="mb-3">
+                <label for="txbMensajeForm" class="form-label">Mensaje:</label>
+                <asp:TextBox ID="txbMensajeForm" CssClass="form-control" runat="server" TextMode="MultiLine"></asp:TextBox>
+            </div>
+            <div class="d-grid gap-2">
+                <asp:Button ID="btnEnviarMensaje" CssClass="btn btn-primary" runat="server" type="submit" Text="Envíar" />
+            </div>
+        </div>
+        <div class="col-2"></div>
+    </div>
+</asp:Content>
---
+++ servicioFreelance/servicioFreelance/Contacto.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace servicioFreelance
+{
+
+
+    public partial class Contacto
+    {
+
+        /// <summary>
+        /// Control txbNombreApellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txbNombreApellido;
+
+        /// <summary>
+        /// Control txbEmailAdress.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txbEmailAdress;
+
+        /// <summary>
+        /// Control txbAsunto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txbAsunto;
+
+        /// <summary>
+        /// Control txbMensajeForm.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txbMensajeForm;
+
+        /// <summary>
+        /// Control btnEnviarMensaje.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEnviarMensaje;
+    }
+}
